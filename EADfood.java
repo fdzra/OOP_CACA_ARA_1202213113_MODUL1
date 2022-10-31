7c077648c43b33e8baa1a6fa8bce128699812306
fdzra <96951041+fdzra@users.noreply.github.com>
2022-10-31 11:11:03 +0700

    Add files via upload

---
+++ EADfood.java
@@ -0,0 +1,7 @@
+public class EADfood {
+    public static void EADfood(string[]args) {
+
+        system.out.println("Selamat Datang di Restoran EAD");
+        system.out.println("Silahkan Register Terlebih Dahulu");
+
+}
---
+++ Server.java
@@ -0,0 +1,19 @@
+public class Server {
+    
+    String menu;
+    String pilihmenu;
+    String keluar;
+
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+
+    public void setPilihMenu(String pilihmenu) {
+        this.pilihmenu = pilihmenu;
+    }
+
+    public void setKeluar(String keluar) {
+        this.keluar = keluar;
+    }
+
+}
