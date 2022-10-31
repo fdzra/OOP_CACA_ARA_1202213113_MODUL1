4b664e44c831c9442c5c16ee8a473e38d68be731
fdzra <96951041+fdzra@users.noreply.github.com>
2022-10-31 11:11:28 +0700

    Delete EADfood.java

--- EADfood.java
+++
@@ -1,7 +0,0 @@
-public class EADfood {
-    public static void EADfood(string[]args) {
-
-        system.out.println("Selamat Datang di Restoran EAD");
-        system.out.println("Silahkan Register Terlebih Dahulu");
-
-}
